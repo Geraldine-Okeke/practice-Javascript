477f1af2f0260431710998a76bb0d0c899357ce6
Geraldine-Okeke <me.ejiofor@unizik.edu.ng>
2023-05-19 10:52:14 +0100

    spread

---
+++ spread-operators/sp-op1.js
@@ -0,0 +1,7 @@
+let states = ["Anambra","Abuja","Lagos","Portharcout"]
+
+function places(a,b,c,d){
+  console.log(`visit ${a}, it's a good place , you can also try ${b},${c} and ${d}`)
+
+}
+places(...states)
